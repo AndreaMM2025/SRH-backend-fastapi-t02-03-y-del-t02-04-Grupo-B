95999e52d93a9662d0b574740c683f5e665e9d34
Andrea Murillo <andreamurillomedina110@gmail.com>
2025-12-20 04:35:42 -0500

    feat: schema reserva

---
+++ app/schemas/reserva_schema.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+
+class ReservaCreate(BaseModel):
+    cliente_id: int
+    habitacion_id: int
+    fecha_inicio: str
+    fecha_fin: str
+
+class ReservaResponse(ReservaCreate):
+    id: int
+    estado: str
