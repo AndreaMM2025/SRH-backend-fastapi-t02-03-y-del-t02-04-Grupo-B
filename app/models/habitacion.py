2a45035cb6676794fc28e7f7fb6bf1a6630bbf88
Andrea Murillo <andreamurillomedina110@gmail.com>
2025-12-20 04:26:06 -0500

    feat: modelo habitacion

---
+++ app/models/habitacion.py
@@ -0,0 +1,9 @@
+from typing import Optional
+from pydantic import BaseModel
+
+class Habitacion(BaseModel):
+    id: Optional[int] = None
+    numero: str
+    tipo: str
+    precio: float
+    disponible: bool = True
