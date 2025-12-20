e5614f540a3ff8b2327696d4430835fc89e7ff7c
Andrea Murillo <andreamurillomedina110@gmail.com>
2025-12-20 04:18:40 -0500

    feat: registrar router clientes

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from app.routers import clientes
 
 app = FastAPI(title="SRH Backend")
 
 @app.get("/")
 def root():
     return {"message": "API funcionando correctamente"}
+
+app.include_router(clientes.router)
