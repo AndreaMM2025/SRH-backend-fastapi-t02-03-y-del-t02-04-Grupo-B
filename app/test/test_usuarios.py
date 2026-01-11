00db9b953b8030c0453ccee64069ddea72b65ab0
David-11-design <arevaloandy073@gmail.com>
2026-01-11 18:29:27 -0500

    Creacion del rol del cliente 2

--- app/test/test_usuarios.py
+++ app/test/test_usuarios.py
@@ -23,6 +23,26 @@ def test_crear_usuario_1():
     # Validamos que el backend genere un id
     assert "id" in data
 
+def test_crear_usuario_2():
+    usuario = {
+        "nombre": "David",
+        "rol": "Recepcionista",
+        "estado": True
+    }
+
+    response = client.post("/api/usuarios/", json=usuario)
+    assert response.status_code == 200
+
+    data = response.json()
+
+    # Validamos campos principales
+    assert data["nombre"] == "David"
+    assert data["rol"] == "Recepcionista"
+    assert data["estado"] is True
+
+    # Validamos que el backend genere un id
+    assert "id" in data
+
 def test_listar_usuarios():
     response = client.get("/api/usuarios/")
     assert response.status_code == 200
