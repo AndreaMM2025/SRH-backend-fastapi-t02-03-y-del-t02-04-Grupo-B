c0451de507104eb3304f0e2a1f97bf5e8c80c5bf
Keyla Sisalima <keylasisalimaeaes@gmail.com>
2026-01-12 22:32:29 -0500

    Creación de la factura 4

--- app/test/test_facturas.py
+++ app/test/test_facturas.py
@@ -65,7 +65,29 @@ def test_crear_factura_3():
     assert data["total"] == 300.0
     assert data["fecha"] == "2026-03-05"
 
+
+def test_crear_factura_4():
+    factura = {
+        "cliente_id": 4, 
+        "reserva_id": 4, 
+        "total": 110.0, 
+        "fecha": "2026-01-23"
+    }
+    response = client.post("/api/facturas/", json=factura)
+    
+    assert response.status_code == 200, response.text
+
+    data = response.json()
+    assert data["cliente_id"] == 4
+    assert data["reserva_id"] == 4
+    
+    assert data["total"] == 110.0
+    assert data["fecha"] == "2026-01-23"
+
+
+
 def test_listar_facturas():
     response = client.get("/api/facturas/")
     assert response.status_code == 200, response.text
     assert isinstance(response.json(), list)
+    
