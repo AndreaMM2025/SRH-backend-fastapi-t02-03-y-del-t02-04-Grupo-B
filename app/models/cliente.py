4bbd7f07570e8ac0c5db108266146a860add4f28
Andrea Murillo <andreamurillomedina110@gmail.com>
2026-02-22 00:36:51 -0500

    actualizar campos en el swagger

--- app/models/cliente.py
+++ app/models/cliente.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel, EmailStr
 class Cliente(BaseModel):
     id: Optional[int] = None
     nombre: str
-    identificacion: str
+    identidad: str
     telefono: str
     correo: EmailStr
     nacionalidad: str
